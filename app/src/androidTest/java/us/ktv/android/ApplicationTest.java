5ed87988f1a19efc22b444e756d79f9a8f8c3d0e
justnick <findnick@163.com>
2015-10-10 18:15:07 +0800

    update room test

--- app/src/androidTest/java/us/ktv/android/ApplicationTest.java
+++ app/src/androidTest/java/us/ktv/android/ApplicationTest.java
@@ -23,9 +23,9 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
             "}";
 
     private static final String TEXT_DATA_ROOM_JSON = "{\n" +
-            "\"id\":\"MSEFAPWO\",\n" +
-            "\"name\":\"YoYo\",\n" +
-            "\"add_time\":\"2015/10/8\"\n" +
+            "    \"id\":\"192.168.1.1:8989\",\n" +
+            "    \"name\":\"192.168.1.1:8989\",\n" +
+            "    \"add_time\":\"2015/10/8\"\n" +
             "}";
 
     public ApplicationTest() {
--- database/src/main/java/us/ktv/database/datamodel/Room.java
+++ database/src/main/java/us/ktv/database/datamodel/Room.java
@@ -8,8 +8,8 @@ import com.google.gson.annotations.SerializedName;
 public class Room {
     /**
      * {
-     "id":"MSEFAPWO",
-     "name":"YoYo",
+     "id":"192.168.1.1:8989",
+     "name":"192.168.1.1:8989",
      "add_time":"2015/10/8"
      }
      */
